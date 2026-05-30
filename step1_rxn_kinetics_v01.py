b1093f72fcef26774fc01c65d44d8baacbb5d677
sebygaa <63217093+sebygaa@users.noreply.github.com>
2026-05-30 21:33:52 +0900

    Update step1_rxn_kinetics_v01.py

--- step1_rxn_kinetics_v01.py
+++ step1_rxn_kinetics_v01.py
@@ -8,12 +8,12 @@ from scipy.integrate import odeint
 # %%
 # Defning Key Variables
 # %%
-kd_test = 0.1
-kp_test = 0.05
-ktc_test = 0.01
-ktd_test = 0.01
-ktrm_test = 0.01
-ktrp_test = 0.01
+kd_test = 10
+kp_test = 5
+ktc_test = 1
+ktd_test = 1
+ktrm_test = 1
+ktrp_test = 1
 
 f_test = 0.8 # Initiator efficiency (80% assumed)
 
@@ -23,7 +23,7 @@ par_list = [kd_test, kp_test, ktc_test, ktd_test,
 
 # %%
 # Form of ODE equations
-mu3 = 200
+#mu3 = 200
 def rxn1(y,t,arg_list):
     lamb0 = y[0]
     lamb1 = y[1]
@@ -38,7 +38,8 @@ def rxn1(y,t,arg_list):
 
     # mu3 from Schulz-Zimm distribution (gamma distribution assumptio)
     # Hulburt & Katz approximation
-    m3 = mu2*(2*mu0*mu2 - mu1**2)/mu0/mu1
+    mu3_guess = mu2*(2*mu0*mu2 - mu1**2)/(mu0+1E-6)/(mu1+1E-6)
+    mu3 = min(mu3_guess, 5000)
     
     # Initiator and monomor consumption
     dini_dt = -kd*ini
@@ -50,8 +51,9 @@ def rxn1(y,t,arg_list):
     # lamb1
     term1_1 = kp*mono*lamb0
     term1_2 = ktrm*mono*(lamb0-lamb1)
-    term1_3 = -(ktc+ktd)*lamb0*lamb1
-    dlamb1_dt =  term1_1 + term1_2 + term1_3
+    term1_3 = ktrp*(lamb0*mu2 - lamb1*mu1)
+    term1_4 = -(ktc+ktd)*lamb0*lamb1
+    dlamb1_dt =  term1_1 + term1_2 + term1_3+term1_4
     # lamb2
     term2_1 = kp*mono*(2*lamb1+lamb0)
     term2_2 = ktrm*mono*(lamb0 - lamb2)
@@ -63,15 +65,18 @@ def rxn1(y,t,arg_list):
     # mu0
     term3_1 = ktrm*mono*lamb0
     term3_2 = (0.5*ktc+ktd)*lamb0**2
-    term3_3 = -ktrp*lamb0*mu1
-    dmu0_dt = term3_1 + term3_2 + term3_3
+    # term3_3 = -ktrp*lamb0*mu1 # Claude 가 찾은 오류
+    dmu0_dt = term3_1 + term3_2 
     # mu1
     dmu1_dt = ktrm*mono*lamb1 + (ktc+ktd)*lamb0*lamb1
+    dmu1_dt += ktrp*(lamb1*mu1 - lamb0*mu2) # Claude가 찾은 오류
     # mu2
     term4_1 = ktrm*mono*lamb2
-    term4_2 = ktd*lamb0+lamb2
+    term4_2 = ktd*lamb0*lamb2
     term4_3 = ktc*(lamb0*lamb2+lamb1**2)
     dmu2_dt = term4_1 +term4_2+term4_3
+    dmu2_dt += ktrp*(mu1*lamb2 - lamb0*mu3)
+
     dydt_list = [dlamb0_dt, dlamb1_dt, dlamb2_dt,
                  dmu0_dt, dmu1_dt, dmu2_dt,
                  dini_dt, dmono_dt]
@@ -94,7 +99,7 @@ y_res = odeint(rxn1,y0,t_ran, args = (par_list,),)
 # Graph
 
 # %%
-plt.plot(t_ran, y_res[:,4])
+plt.plot(t_ran, y_res[:,0])
 # %%
 plt.plot(t_ran, y_res[:,-1])
 # %%
